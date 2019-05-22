ea415e9a03414cb500440660a4a539158bf85cae
geetha110 <50898694+geetha110@users.noreply.github.com>
2019-05-22 17:24:02 +0530

    Create guvi.py

---
+++ guvi.py
@@ -0,0 +1,9 @@
+a=int(input("enter n value"))
+if(a>0):
+  print("the given number is positive value")
+elif(a<0):
+  print("the given number is negative value ")
+elif(a==0):
+  print("the given number is 0")
+else:
+  print("invalid input")
